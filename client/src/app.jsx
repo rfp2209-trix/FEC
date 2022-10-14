d8a651049080d0eff7f69c89fd3fdbd45aff6ab6
Cole Bienek <colebienek@gmail.com>
2022-10-14 16:21:20 -0700

    del nvm

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable import/extensions */
 import React, {useContext, useState, useEffect} from 'react';
-import axios from 'axios';
 import ProductsOverview from './overview/productsOverview.jsx';
-import {ProductContext, Context} from './Context.js';
 
 import Reviews from './reviews/Reviews.jsx';
 import WriteReview from './reviews/WriteReview.jsx';
