c8eb3ce385619d2a5c90d9a1d6e38aedc5831d0c
nhasty <nyall.hasty@gmail.com>
2022-10-17 21:13:41 -0700

    helpful, report, more reviews clicks all functional

--- client/src/reviews/Reviews.jsx
+++ client/src/reviews/Reviews.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import ReviewsMeta from './ReviewsMeta.jsx';
 import ReviewsList from './ReviewsList.jsx';
 
-function Reviews() {
+function Reviews({setAddForm}) {
   return (
-    <div>
+    <ReviewsContainer>
       <h2>RATINGS & REVIEWS</h2>
       <ReviewsMeta />
       <ReviewsList />
-    </div>
+    </ReviewsContainer>
   );
 }
 
 export default Reviews;
+
+export const ReviewsContainer = styled.div`
+
+  width: 100vw;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  flex: 1;
+`;
--- client/src/reviews/ReviewsList.jsx
+++ client/src/reviews/ReviewsList.jsx
@@ -1,25 +1,67 @@
 import React from 'react';
+import axios from 'axios';
 import ReviewTile from './ReviewTile.jsx';
 import { useProductsContext } from '../Context.jsx';
-// cometnt
+
 function ReviewsList() {
-  const { totalReviews, loading } = useProductsContext();
+  const { reviews, totalReviews, setState, state, loading } = useProductsContext();
 
   if (loading) {
     return (
       <div />
     );
   }
+  const reviewListComponents = reviews.results.map((review) => (
+    <ReviewTile key={review.review_id} review={review} />
+  ));
   return (
     <div>
-      <div>
+      <label htmlFor="sort_by">
         {totalReviews}
-        &nbsp;reviews, sorted by (insert dropdown here)
-      </div>
+        &nbsp;reviews, sorted by&nbsp;
+        <select
+          id="select_sort"
+          name="sort_by"
+          onChange={(event) => {
+            axios.get(`/fec/reviews?product_id=${reviews.product}&count=2&sort=${event.target.value.toLowerCase()}`)
+              .then((apiResponse) => {
+                setState({
+                  ...state,
+                  reviews: apiResponse.data,
+                });
+              })
+              .catch((err) => console.log(err));
+          }}
+        >
+          <option value="Relevent">Relevent</option>
+          <option value="Helpful">Helpful</option>
+          <option value="Newest">Newest</option>
+        </select>
+      </label>
       <input type="text" placeholder="keyword search (low priority)" />
       <button type="button">Search!</button>
-      <ReviewTile />
-      {totalReviews > 2 && <button type="button">MORE REVIEWS</button>}
+      <ol>
+        {reviewListComponents}
+      </ol>
+      {totalReviews > reviewListComponents.length && (
+        <button
+          type="button"
+          onClick={() => {
+            console.log(reviews.page)
+            axios.get(`/fec/reviews?product_id=${reviews.product}&count=2&sort=${document.getElementById('select_sort').value.toLowerCase()}&page=${reviews.page ? ((reviews.page / 2) + 2) : 2}`)
+              .then((apiResponse) => {
+                reviews.page = apiResponse.data.page;
+                reviews.results.push(...apiResponse.data.results);
+                setState({
+                  ...state,
+                });
+              })
+              .catch((err) => console.log(err));
+          }}
+        >
+          MORE REVIEWS
+        </button>
+      )}
       <button type="button">ADD REVIEW</button>
       <div>
         REMOVE ME!
