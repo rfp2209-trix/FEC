af45d66cbb69328bd78cc38a2a7027226d3b985c
Cole Bienek <colebienek@gmail.com>
2022-10-26 11:44:44 -0700

    added css optimizer to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,43 @@
-const path = require('path')
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  entry: "./client/index.jsx",
+  entry: './client/index.jsx',
   output: {
-    path: path.resolve(__dirname, "client/dist"),
-    filename: "bundle.js"
-    },
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js',
+  },
   module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use:{
-            loader: 'babel-loader'
-          }
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test: /\.css$/,
-          use: {
-            loader:'style-loader'
-          }
-        }
-      ]
-    }
-}
+      },
+      {
+        test: /\.css$/,
+        use: {
+          loader: 'style-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CompressionPlugin(),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({
+        cssProccesorOptions: {
+          map: {
+            inline: false,
+            annotation: true,
+          },
+        },
+      }),
+    ],
+  },
+};
