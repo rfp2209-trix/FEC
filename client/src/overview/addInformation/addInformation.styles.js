75ccac432e96f51f4ce540fd4b5d09094b95c336
Cole Bienek <colebienek@gmail.com>
2022-10-26 15:12:53 -0700

    added total reviews conditional to stars

---
+++ client/src/overview/addInformation/addInformation.styles.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const AdditionalInfoContainer = styled.div`
+margin: 0;
+padding: 0;
+display: flex;
+flex-direction: column;
+> h1 {
+  font-size: 16pt;
+  line-height: 16pt;
+  height: 20px;
+  margin-top: 10px;
+  margin-bottom: 0;
+}
+.basic-text {
+  margin-top: 10px;
+  line-height: 12pt;
+  font-size: 12pt;
+}
+`;
